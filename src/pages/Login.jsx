e33425c85d025bb5b5aa7627f373883973741202
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-11-16 00:21:21 +0200

    Finished the Login page layout, need to implement functionalities

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import LoginContainer from "../sections/LoginPageSections/LoginContainer";
+
+const Login = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <LoginContainer />
+    </div>
+  );
+};
+
+export default Login;
